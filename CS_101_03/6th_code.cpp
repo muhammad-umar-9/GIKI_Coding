e606dacdf3adb635c01a0ec33d034e9569e6d129
muhammad-umar-9 <muhammadumark900@gmail.com>
2024-05-04 21:40:47 +0500

    by umar

---
+++ CS_101_03/6th_code.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+    int number = 5;
+
+    int array[6] = {1 , 2 , 3 , 4 , 5 };
+
+    for(int i = 0; i < 6; ++i)
+     {                           //here we compare elements of array
+        for(int j = i+1; j<6; ++j) 
+        {
+            if(array[i] + array[j] == number)  //to check condition
+            {
+                cout<<array[i]<<" "<< array[j] << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
+
