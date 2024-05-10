825bf248b683599da2bc4acc16ec76de23d86e89
muhammad-umar-9 <muhammadumark900@gmail.com>
2024-05-10 20:42:34 +0500

    done

--- CS_101_03/9th_code.cpp
+++ CS_101_03/9th_code.cpp
@@ -4,97 +4,89 @@ using namespace std;
 
 int main()
  {
-    int n1, n2;
+    int num_1, num_2;
     cout<<"Enter Size of Ist array"<<endl;
-    cin>>n1;
-    int *arr1 = new int[n1];
+    cin>>num_1;
+    int *array1 = new int[num_1];
     
     cout<<"Enter elements of array 1 "<<endl;
-   
-    for(int i = 0; i < n1; i++)
+    for(int i =0; i<num_1; i++)
      {
-        cin >> arr1[i];
+        cin>>array1[i];
     }
     
     cout<<"Enter Size of 2nd array "<<endl;
-    cin>>n2;
-    int *arr2 = new int[n2];  //here we use pointers
+    cin>>num_2;
+    int *array2 = new int[num_2];  //here we use pointers
     
     cout<<"Enter elements of array 2 "<<endl;
-    for (int j = 0; j < n2; j++) 
+    for(int j=0; j<num_2; j++) 
     {
-        cin >> arr2[j];
+        cin>>array2[j];
     }
 
     int i=0; 
     int j=0;
     
-    
     int *s_u;
     int *s_i;
-    s_u= new int[n1 + n2];
+    s_u = new int[num_1 + num_2];
  
-    s_i =new int[min(n1, n2)]; //here we use built in function min
+    s_i = new int[min(num_1, num_2)]; //here we use built in function min
     
     
-    int u_size = 0;
-    int i_size = 0;
+    int u_size =0;
+    int i_size =0;
     
-    // Use a two-pointer approach to compute the sorted union and intersection
-    while (i < n1 && j < n2)
+    
+    while(i<num_1 && j<num_2)
      {
-        if (arr1[i] < arr2[j]) 
+      if(array1[i]<array2[j]) 
         {
             
-            s_u[u_size++] = arr1[i];
+            s_u[u_size++] = array1[i];
             i++;
         }
-         else if (arr1[i]> arr2[j]) 
+      else if(array1[i]> array2[j]) 
          {
             
-            s_u[u_size++]=arr2[j];
+            s_u[u_size++]=array2[j];
             j++;
         } 
-        else 
+      else 
         {
-            
-            s_u[u_size++]=arr1[i];
-            s_i[i_size++]=arr1[i];
+            s_u[u_size++]=array1[i];
+            s_i[i_size++]=array1[i];
             i++;
             j++;
         }
     }
-    while (i<n1) 
+    while (i<num_1) 
     {
-        s_u[u_size++]=arr1[i];
+        s_u[u_size++]=array1[i];
         i++;
     }
-    
-    
-    while (j<n2) 
+    while(j<num_2) 
     {
-        s_u[u_size++] = arr2[j];
+        s_u[u_size++] = array2[j];
         j++;
     }
     
-    
     cout << "Union Of Elements are: " << endl;
-    for (int k = 0; k < u_size; k++) 
+    for (int k=0; k<u_size; k++) 
     {
-        cout<< s_u[k] << " ";
+        cout<< s_u[k]<<" ";
     }
-    cout<<endl;
+      cout<<"\n";
     
     // Output the size and elements of the sorted intersection array
     
     cout<<"Intersection Of Elements are : ";
-    for (int k = 0; k < i_size; k++) 
+    for (int k=0; k<i_size; k++) 
     {
-        cout << s_i[k] << " ";
+        cout<<s_i[k]<<" ";
     }
-    cout << endl;
-    
+    cout<<"\n";
 
-    
     return 0;
 }
