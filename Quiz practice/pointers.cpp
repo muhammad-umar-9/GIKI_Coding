6af7d7f1bb93d8ad5bec4f3821e70e92767bd714
muhammad-umar-9 <muhammadumark900@gmail.com>
2024-05-14 07:29:25 +0500

    done

---
+++ Quiz practice/pointers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+      int var1;
+      char var2[10];
+      cout<<"Adress of Var1 "<<endl;
+      cout<< &var1 <<endl;
+      cout<<"Adress of Var2 "<<endl;
+      cout<< &var1 <<endl;
+      return 0;
+}
