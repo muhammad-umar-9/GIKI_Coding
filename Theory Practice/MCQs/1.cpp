6a409d68b6c6b43454408bff2acb7af3c77ac81e
muhammad-umar-9 <muhammadumark900@gmail.com>
2024-05-20 07:09:14 +0500

    mcqs

---
+++ Theory Practice/MCQs/1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Base{
+	int _x;
+public:
+	Base() {}
+	Base(int x):_x{x} {}
+	Base(const Base& obj) { _x = obj._x; }
+	friend ostream& operator << (ostream& os, const Base obj) {
+		os << obj._x;
+		return os;
+	}
+};
+int main() {
+	Base b1(10);
+	Base b2 = b1;
+	cout << b2 << endl;
+	return 0;
+}
